de70e73dafb286a848bebdeef7664ca62269e2d8
renato <renato@twobo.com>
2017-08-03 22:06:52 +0200

    v6 Kotlin coroutines

--- src/main/kotlin/max_column_sum_by_key.kt
+++ src/main/kotlin/max_column_sum_by_key.kt
@@ -1,6 +1,11 @@
-import java.io.File
-import java.util.HashMap
+@file:Suppress("EXPERIMENTAL_FEATURE_WARNING")
 
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.runBlocking
+import java.io.File
+import java.io.RandomAccessFile
+import java.util.HashMap
 
 fun main(arguments: Array<String>) {
     when (arguments.size) {
@@ -48,11 +53,51 @@ private fun ByteArray.toIntUpTo(maxIndex: Int): Int {
 
 const val delim = '\t'
 
-private fun run(file: File, keyIndex: Int, valueIndex: Int) {
+private fun run(file: File, keyIndex: Int, valueIndex: Int) = runBlocking {
     val maxFieldIndex = maxOf(keyIndex, valueIndex)
-    val sumByKey = HashMap<String, IntBox>()
 
-    val inputStream = file.inputStream()
+    val partition1 = RandomAccessFile(file, "r")
+    val partition2 = RandomAccessFile(file, "r")
+
+    val fileLength = file.length()
+    var secondPartitionStartIndex = fileLength / 2L
+
+    partition2.seek(secondPartitionStartIndex)
+
+    while (true) {
+        val byte = partition2.read()
+        secondPartitionStartIndex++
+        if (byte < 0 || byte.toChar() == '\n') {
+            break
+        }
+    }
+
+    val firstPartitionMaxEntry = async(CommonPool) {
+        maxOfPartition(partition1, keyIndex, valueIndex, maxFieldIndex, secondPartitionStartIndex.toInt())
+    }
+
+    val secondPartitionMaxEntry = async(CommonPool) {
+        maxOfPartition(partition2, keyIndex, valueIndex, maxFieldIndex, fileLength.toInt())
+    }
+
+    val maxEntry = listOf(firstPartitionMaxEntry, secondPartitionMaxEntry)
+            .map { it.await() }
+            .maxBy { it.component2().int }
+
+    if (maxEntry == null || maxEntry.first.isEmpty()) {
+        println("No entries")
+    } else {
+        val (key, value) = maxEntry
+        println("max_key: $key, sum: $value")
+    }
+}
+
+private suspend fun maxOfPartition(partition: RandomAccessFile,
+                                   keyIndex: Int,
+                                   valueIndex: Int,
+                                   maxFieldIndex: Int,
+                                   lastByteIndex: Int): Pair<String, IntBox> {
+    val sumByKey = HashMap<String, IntBox>()
 
     val buffer = ByteArray(1024 * 1_000)
     var fieldIndex = 0
@@ -70,46 +115,44 @@ private fun run(file: File, keyIndex: Int, valueIndex: Int) {
         currentValMaxIndex = 0
     }
 
-    inputStream.use {
-        while (true) {
-            val bytesCount = inputStream.read(buffer)
+    while (true) {
+        val bytesCount = partition.read(buffer)
 
-            if (bytesCount < 0) {
-                break
-            }
+        if (bytesCount < 0) {
+            break
+        }
 
-            (0 until bytesCount).forEach { i ->
-                val byte = buffer[i]
-                val char = byte.toChar()
+        (0 until bytesCount).forEach { i ->
+            val byte = buffer[i]
+            val char = byte.toChar()
 
-                if (fieldIndex <= maxFieldIndex) {
-                    when (char) {
-                        delim -> {
-                            fieldIndex++
-                        }
-                        '\n' -> {
-                            startLine()
-                        }
-                        else -> {
-                            if (fieldIndex == keyIndex) {
-                                currentKey.append(char)
-                            } else if (fieldIndex == valueIndex) {
-                                currentVal[currentValMaxIndex++] = byte
-                            }
+            if (fieldIndex <= maxFieldIndex) {
+                when (char) {
+                    delim -> {
+                        fieldIndex++
+                    }
+                    '\n' -> {
+                        startLine()
+                    }
+                    else -> {
+                        if (fieldIndex == keyIndex) {
+                            currentKey.append(char)
+                        } else if (fieldIndex == valueIndex) {
+                            currentVal[currentValMaxIndex++] = byte
                         }
                     }
-                } else if (char == '\n') {
-                    startLine()
                 }
+            } else if (char == '\n') {
+                startLine()
             }
+        }
+
+        if (partition.filePointer.toInt() + buffer.size >= lastByteIndex) {
+            println("Exiting early at position ${partition.filePointer}")
+            break // we might have read a few too many bytes, but by now we can definitely exit
         }
     }
 
-    if (sumByKey.isEmpty()) {
-        println("No entries")
-    } else {
-        val (key, maxEntry) = sumByKey.maxBy { it.value.int }!!
-        println("max_key: $key, sum: ${maxEntry.int}")
-    }
+    return sumByKey.maxBy { it.value.int }?.toPair() ?: ("" to IntBox())
 }
 
